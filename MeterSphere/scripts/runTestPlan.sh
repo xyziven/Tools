b853548a43f9443c8500a7beb371c5536c9684d3
xyziven <xyziven@gmail.com>
2024-08-30 21:29:29 +0800

    Update runTestPlan.sh

--- MeterSphere/scripts/runTestPlan.sh
+++ MeterSphere/scripts/runTestPlan.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+######################################################
+#### This script is to trigger MeterSphere Test Plan #
+####                                                 #
+#### Author Ziven.shy                                #
+#### 2024.8.30                                       #
+####                                                 #
+#### modified on 2024 Aug 29                         #
+#### replace HOST with HOST URL                      #
+####                                                 #
+#### Copyright @2024                                 # 
+######################################################
+
 usage()
 {
  echo Usage: sh $0 accessKey secretKy projectName envName testPlanName HOST_URL resourcePoolName userId
@@ -13,11 +25,10 @@ else
   projectName=${3}
   envName=${4}
   testPlanName=${5}
-  HOST=${6}
+  HOST_URL=${6}
   resourcePoolName=${7}
   userId=${8}
 fi
-
 
 projectId="projectId"
 envId="environmentId"
@@ -36,7 +47,7 @@ signature=$(printf %s "${seed}" | openssl enc -e -aes-128-cbc -base64 -K ${keySp
 # get Project ID through project name
 getProjectId()
 {
-    RESULT=$(curl -k -s -H "accesskey: ${accessKey}" -H "signature: ${signature}" -H "Content-Type: application/json" https://${HOST}/project/project/list/all)
+    RESULT=$(curl -k -s -H "accesskey: ${accessKey}" -H "signature: ${signature}" -H "Content-Type: application/json" ${HOST_URL}/project/project/list/all)
     projectId=$(printf '%s\n' "${RESULT}" | jq '.data[] | select(.name == "'"${projectName}"'")' |jq .id)
 }
 
@@ -44,9 +55,11 @@ getEnvId()
 {
         proj_id=$1
         BODY='{"projectIds":['$proj_id']}'   
-        RESULT=$(curl -k -s -X POST -H "accesskey: ${accessKey}" -H "signature: ${signature}" -H "Content-Type: application/json" https://${HOST}/project/environment/list/1/10 -d "${BODY}")
+        RESULT=$(curl -k -s -X POST -H "accesskey: ${accessKey}" -H "signature: ${signature}" -H "Content-Type: application/json" ${HOST_URL}/project/environment/list/1/10 -d "${BODY}")
 #        echo ${RESULT}
         envId=$(printf '%s\n' "${RESULT}" | jq '.data.listObject[] | select(.name == "'"${envName}"'")' |jq .id)
+
+
 }
 
 # get Test Plan ID through plan name
@@ -55,7 +68,7 @@ getTestPlanId()
     proj_id=$1
     plan_name=$2
     BODY='{"projectId": '$proj_id'}'
-    RESULT=$(curl -k -s -X POST -H "accesskey: ${accessKey}" -H "signature: ${signature}" -H "Content-Type: application/json" https://${HOST}/track/test/plan/list/1/1000 -d "${BODY}")
+    RESULT=$(curl -k -s -X POST -H "accesskey: ${accessKey}" -H "signature: ${signature}" -H "Content-Type: application/json" ${HOST_URL}/track/test/plan/list/1/1000 -d "${BODY}")
     testPlanId=$(printf '%s\n' "${RESULT}" | jq '.data.listObject[] | select(.name == "'"${plan_name}"'")' |jq .id)
 }
 
@@ -63,9 +76,9 @@ getTestPlanId()
 getResourcePoolId()
 {
   BODY='{"name":"'$resourcePoolName'"}'
-  # CMD="curl -k -s -H \"accesskey: ${accessKey}\" -H \"signature: ${signature}\" -H \"Content-Type: application/json\" -X POST https://${HOST}/setting/testresourcepool/list/1/10 -d \'${BODY}\'"
+  # CMD="curl -k -s -H \"accesskey: ${accessKey}\" -H \"signature: ${signature}\" -H \"Content-Type: application/json\" -X POST ${HOST_URL}/setting/testresourcepool/list/1/10 -d \'${BODY}\'"
   # echo ${CMD}
-  RESULT=$(curl -k -s -H "accesskey: ${accessKey}" -H "signature: ${signature}" -H "Content-Type: application/json" -X POST https://${HOST}/setting/testresourcepool/list/1/10 -d ${BODY})
+  RESULT=$(curl -k -s -H "accesskey: ${accessKey}" -H "signature: ${signature}" -H "Content-Type: application/json" -X POST ${HOST_URL}/setting/testresourcepool/list/1/10 -d ${BODY})
   # echo "${RESULT}"
   resourcePoolId=$(printf '%s\n' "${RESULT}" | jq '.data.listObject[] |  select(.name == "'"${resourcePoolName}"'")' |jq .id)
 }
@@ -83,8 +96,9 @@ runTestPlan()
 
    BODY='{ "mode": "serial", "reportType":"iddReport", "onSampleError":false, "requestOriginator":"TEST_PLAN", "executionWay":"RUN", "testPlanId":'${plan_id}',"projectId":'${proj_id}',"triggerMode": "API","userId":"'${user_id}'","runWithinResourcePool": true, "resourcePoolId":'${rspool_id}', "environmentType":"JSON","environmentGroupId":"","envMap":{}}'
 
-    RESULT=$(curl -k -s -X POST -H "accessKey: ${accessKey}" -H "signature: ${signature}" -H "Content-Type: application/json" https://${HOST}/track/test/plan/run -d "${BODY}")
-    sleep 1
+#    echo "CMD is: curl -k -s -X POST -H \"accessKey: ${accessKey}\" -H \"signature: ${signature}\" -H \"Content-Type: application/json\" ${HOST_URL}/track/test/plan/run -d \'${BODY}\' "
+    RESULT=$(curl -k -s -X POST -H "accessKey: ${accessKey}" -H "signature: ${signature}" -H "Content-Type: application/json" ${HOST_URL}/track/test/plan/run -d "${BODY}")
+    echo "Result: $RESULT "
     execResult=$(echo $RESULT | jq -r '.success')
     if [[ $execResult == true ]];then
       echo "success: true"
@@ -94,7 +108,7 @@ runTestPlan()
       echo "Failed to call API to run the test plan"
       echo "RESULT: $RESULT"
       echo "PLEASE check the CMD:"
-      echo "... curl -k -s -X POST -H \"accessKey: ${accessKey}\" -H \"signature: ${signature}\" -H \"Content-Type: application/json\" https://${HOST}/track/test/plan/run -d \'${BODY}\' "
+      echo "... curl -k -s -X POST -H \"accessKey: ${accessKey}\" -H \"signature: ${signature}\" -H \"Content-Type: application/json\" ${HOST_URL}/track/test/plan/run -d \'${BODY}\' "
     fi
 }
 
@@ -103,27 +117,29 @@ getReportSharedId()
     report_id=$1
     BODY='{"customData":"'${report_id}'","shareType":"PLAN_DB_REPORT","lang":null}'
 
-    RESULT=$(curl -k -s -X POST -H "accessKey: ${accessKey}" -H "signature: ${signature}" -H 'Content-Type: application/json' https://${HOST}/track/share/generate/expired -d "${BODY}")
+    getReportShareIDCMD="curl -k -s -H \"accesskey: ${accessKey}\" -H \"signature: ${signature}\" -H \"Content-Type: application/json\" -X POST ${HOST_URL}/track/share/generate/expired -d \'${BODY}\'"
+    echo "getReportShareIDCMD is: ${getReportShareIDCMD}"
+    RESULT=$(curl -k -s -X POST -H "accessKey: ${accessKey}" -H "signature: ${signature}" -H 'Content-Type: application/json' ${HOST_URL}/track/share/generate/expired -d "${BODY}")
     reportSharedId=$(echo $RESULT | jq -r '.data.shareUrl')
 }
 
 getProjectId
-echo "project_ID: ${projectId}"
+echo "PROJECT_ID: ${projectId}"
 
 getEnvId $projectId
-echo "env_ID: ${envId}"
+echo "Env_ID: ${envId}"
 
 getTestPlanId $projectId ${testPlanName}
-echo "testPlan_ID: ${testPlanId}"
+echo "TestPlanId: ${testPlanId}"
 
 getResourcePoolId ${resourcePoolName}
-echo "resourcePool_ID: $resourcePoolId"
+echo "resource pool Id: $resourcePoolId"
 
 runTestPlan $projectId $testPlanId $envId $resourcePoolId $userId
 getReportSharedId $testReportId
 
 #echo ${reportSharedId}
 
-REPORTURL="https//${HOST}/track/share-plan-report${reportSharedId}"
-echo "Please visit URL: ${REPORTURL} for test report" 
+REPORTURL="${HOST_URL}/track/share-plan-report${reportSharedId}"
+echo "Please visit URL: ${REPORTURL} for test report"
 
